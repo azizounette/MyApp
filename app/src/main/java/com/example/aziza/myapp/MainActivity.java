5c153ffe14e31a98fb03d37574ad44332a6f6390
Aziza <a.l929124@gmail.com>
2017-12-26 20:04:43 +0100

    Added method toGray()
    Method toGray() is used in order to set an image to black and white.

--- app/src/main/java/com/example/aziza/myapp/MainActivity.java
+++ app/src/main/java/com/example/aziza/myapp/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.aziza.myapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.graphics.Color.valueOf;
 
 public class MainActivity extends AppCompatActivity {
 
     private Bitmap bmp;
 
+
+    private void toGray(Bitmap bmp) {
+        int outH = bmp.getHeight();
+        int outW = bmp.getWidth();
+
+        int pixels[] = new int[outW*outH];
+        bmp.getPixels(pixels, 0, outW, 0, 0, outW, outH);
+        for (int i = 0; i < outW*outH; i++) {
+            int gray = (int) (0.11 * Color.blue(pixels[i]) + 0.3 * Color.red(pixels[i]) + 0.59 * Color.green(pixels[i]));
+            pixels[i] = Color.rgb(gray, gray, gray);
+        }
+        bmp.setPixels(pixels, 0, outW, 0, 0, outW, outH);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +50,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return true;
+        ImageView iv = findViewById(R.id.viewLenna);
+        switch(item.getItemId()) {
+            case R.id.toGray:
+                toGray(bmp);
+                iv.setImageBitmap(bmp);
+                Toast.makeText(MainActivity.this, "clicked toGray", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
     
 }
