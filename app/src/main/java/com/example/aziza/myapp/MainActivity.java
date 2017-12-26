39df48d828cd945ff1df1dd17ef89b0281b38b47
Aziza <a.l929124@gmail.com>
2017-12-26 19:37:59 +0100

    Added ImageView, initialised Bitmap
    Added an image to resources. Put it in an ImageView.  Its description is a String that can be found in strings.xml. Created a Bitmap from the image.

--- app/src/main/java/com/example/aziza/myapp/MainActivity.java
+++ app/src/main/java/com/example/aziza/myapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.aziza.myapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Bitmap bmp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv = findViewById(R.id.viewLenna);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.lenna);
+        bmp = bm.copy(bm.getConfig(), true);
+        iv.setImageBitmap(bmp);
     }
 
     
